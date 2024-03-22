5f1dc92a18506c2c46a9f7790384126a844f844f
ThurainTun <stp7112004@gmail.com>
2024-03-22 22:46:13 +0630

    sixth_commit

--- sandbox.ts
+++ sandbox.ts
@@ -1,25 +1,19 @@
-let character: string;
-let age: number;
-let isLoggedIn: true;
+let age: any = 25;
 
-let ninjas: string[] = [];
+age = true;
+console.log(age);
+age = 'thurain';
+console.log(age);
+age = 20;
+console.log(age);
 
-ninjas.push('thurain');
+let mixed: any[] = [];
 
-let mixed: (string | number | boolean)[] = [];
 mixed.push('thurain');
 mixed.push(20);
 mixed.push(true);
 
 console.log(mixed);
-
-let thurainDev: object;
-thurainDev = { name: 'thurain', age: 20 };
-
-let thurainTwo: {
-    name: string,
-    age: number,
-    isBelt: boolean
-}
-
-thurainTwo = { name: 'duat', age: 23, isBelt: true }
+let thurain: { name: any, age: any };
+thurain = { name: 'thurain', age: 20 }
+console.log(thurain);
